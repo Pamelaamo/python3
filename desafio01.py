58084d92b301541dd16b1428a359f12ab5247ce9
Pâmela Amorim <pamelaamorim8@hotmail.com>
2023-08-18 16:58:17 -0300

    desafio01

---
+++ desafio01.py
@@ -0,0 +1,8 @@
+# Faça na tela um programa que leia um numero inteiro e mostre o seu sucessor e seu antecessor
+
+n = int(input('Digite um número:'))
+an = n - 1
+su = n + 1
+print('Analisando o valor {}, o seu antecessor é {}, e o seu sucessor é {}.' .format(n, an, su))
+
+  
