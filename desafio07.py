3465983f69e2d55d4c5232e428e5a2baf944648e
Pâmela Amorim <pamelaamorim8@hotmail.com>
2023-08-18 19:43:18 -0300

    desafio07

---
+++ desafio07.py
@@ -0,0 +1,3 @@
+nome = str(input('Informe seu primeiro nome: '))
+sobreno = str(input('Informe seu sobrenome: '))
+print('Seja bem-vindo(a) ao nosso hotel {} {}, espero que goste da nossa hospitalidade!' .format(nome, sobreno))
